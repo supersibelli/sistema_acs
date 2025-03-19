b9bdccc0532948815bee9babb69ae7e475cae927
supersibelli <supersibelli@gmail.com>
2025-03-19 17:46:52 -0300

    Configurada base de datos para local y producción

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 import os
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv()
 
 class Config:
-    SECRET_KEY = 'tu_clave_secreta_aqui'  # Cambia esto por una clave segura
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+    SECRET_KEY = 'dev'
+    # Usar SQLite localmente
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'instance', 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
